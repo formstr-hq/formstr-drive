a8d01fb504febe2d70f4bd1dcf96a5c6d08a1df2
rama <16651792+geralt-debugs@users.noreply.github.com>
2026-06-11 11:35:47 +0200

    preparing for release

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.formstr.drive",
-  appName: "Drive by Form*",
+  appName: "Drive by Form*(Experimental)",
   webDir: "dist",
 };
 
